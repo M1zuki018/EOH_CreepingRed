6761fde839860cb38a20507f2c69b3eae31a8178
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-03-10 09:35:02 +0900

    SEとVoiceのAudiosourceの管理をオブジェクトプールで行うように変更

--- Assets/Script/Audio/AudioManager.cs
+++ Assets/Script/Audio/AudioManager.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.Pool;
 
 /// <summary>
 /// Audioを管理するManagerクラス
@@ -24,6 +25,9 @@ public class AudioManager : MonoBehaviour
     private List<AudioSource> _seSource; // SE用
     private List<AudioSource>  _voiceSource; // ボイス用
     
+    private IObjectPool<AudioSource> _seSourcePool;
+    private IObjectPool<AudioSource> _voiceSourcePool;
+    
     private void Awake()
     {
         if (Instance != null)
@@ -34,7 +38,59 @@ public class AudioManager : MonoBehaviour
         
         Instance = this;
         DontDestroyOnLoad(gameObject);
+        
+        // SE用のオブジェクトプール初期化
+        _seSourcePool = new ObjectPool<AudioSource>(
+            createFunc: CreateAudioSource,
+            actionOnGet: source => source.gameObject.SetActive(true),
+            actionOnRelease: source => source.gameObject.SetActive(false),
+            actionOnDestroy: Destroy,
+            defaultCapacity: 5,
+            maxSize: 100);
+        
+        // Voice用のオブジェクトプールの初期化
+        _voiceSourcePool = new ObjectPool<AudioSource>(
+            createFunc: CreateAudioSource,
+            actionOnGet: source => source.gameObject.SetActive(true),
+            actionOnRelease: source => source.gameObject.SetActive(false),
+            actionOnDestroy: Destroy,
+            defaultCapacity: 5,
+            maxSize: 20);
     }
+
+    /// <summary>
+    /// オブジェクトプール用
+    /// 新しくGameObjectとAudioSourceを生成する
+    /// </summary>
+    private AudioSource CreateAudioSource()
+    {
+        GameObject obj = new GameObject("PooledAudioSource");
+        obj.transform.SetParent(transform);
+        AudioSource source = obj.AddComponent<AudioSource>();
+        obj.SetActive(false);
+        return source;
+    }
+
+    /// <summary>
+    /// SEのオブジェクトプールからAudioSourceを取得する
+    /// </summary>
+    public AudioSource GetSEAudioSource() => _seSourcePool.Get();
+    
+    /// <summary>
+    /// VoiceのオブジェクトプールからAudioSourceを取得する
+    /// </summary>
+    public AudioSource GetVoiceAudioSource() => _voiceSourcePool.Get();
+
+    /// <summary>
+    /// SEのオブジェクトプールから引数で渡したAudioSourceを解除する
+    /// </summary>
+    public void SESourceRelease(AudioSource source) => _seSourcePool.Release(source);
+    
+    /// <summary>
+    /// Voiceのオブジェクトプールから引数で渡したAudioSourceを解除する
+    /// </summary>
+    public void VoiceSourceRelease(AudioSource source) => _voiceSourcePool.Release(source);
+
 
     /// <summary>
     /// BGMを再生する
@@ -42,6 +98,7 @@ public class AudioManager : MonoBehaviour
     public void PlayBGM(BGMEnum bgm)
     {
         _bgmSource.clip = _bgmClip[bgm];
+        _bgmSource.Play();
     }
 
     /// <summary>
