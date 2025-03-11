767ed323d789c1a51ff6afd3409767b0099ff08b
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-03-11 21:08:30 +0900

    区域のEnum

---
+++ Assets/Script/Map/SectionEnum.cs
@@ -0,0 +1,26 @@
+/// <summary>
+/// 各区域の列挙型
+/// </summary>
+public enum SectionEnum
+{
+    MagicTechnologyResearchArea, // 魔法技術研究区
+    MagicianTrainingCenter, // 魔法士訓練所
+    CentralTower, // セントラル・タワー
+    UpperClassResidentialDistrict, // 上級階級用邸宅街
+    BaysideWarehouse, // 湾岸倉庫
+    LuxuryResidentialDistrictForTheSpecialClass, // 特別階級用高級居住区
+    MiddleClassNorthDistrict, // 中流階級北区
+    CentralTransportHub, // セントラル・トランスポートハブ
+    AltriaArena, // アルテリア・アリーナ
+    BioResearchCenter, // バイオ・リサーチセンター
+    UtilityManagementCenter, // ユーティリティ管理センター
+    MiddleClassSouthDistrict, // 中流階級南区
+    OrderSpiral, // オーダー・スパイラル
+    OrderBazaar, // オーダー・バザール
+    Hollow, // ホロウ
+    WaterCycleManagementPlant, // 水循環管理プラント
+    AltriaAgriculturalDome, // アルテリア農業ドーム
+    AltriaDistributionCenter, // アルテリア配給センター
+    CivilianMagicTrainingCenter, // 民間人魔法訓練所
+    Sea // 海
+}
