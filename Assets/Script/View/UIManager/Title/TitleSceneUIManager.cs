56dc7206af51c778ef9c297bff646fdd065446bf
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-03-13 11:54:20 +0900

    TitleUIManagerを編集

--- Assets/Script/View/UIManager/Title/TitleSceneUIManager.cs
+++ Assets/Script/View/UIManager/Title/TitleSceneUIManager.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 /// </summary>
 public class TitleSceneUIManager : ViewBase
 {
+    [SerializeField] private GameObject[] _windows = new GameObject[3];
+    private IWindow[] _windowsUI = new IWindow[3];
+    
     public override void OnAwake()
     {
         // Canvasコンポーネントの設定
         Canvas canvas = GetComponent<Canvas>();
         canvas.renderMode = RenderMode.ScreenSpaceCamera;
         canvas.worldCamera = Camera.main;
+
+        for(int i = 0; i < 3; i++)
+        {
+            _windowsUI[i] = _windows[i].GetComponent<IWindow>();
+        }
+    }
+
+    public override void OnStart()
+    {
+        //_windowsUI[0].Show();
     }
 }
