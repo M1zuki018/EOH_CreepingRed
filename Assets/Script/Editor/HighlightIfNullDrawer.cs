8f6600d38bdbf53b921b3b5914ecce5f5bd34b93
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-03-21 21:24:38 +0900

    HighlightIfNullDrawerを修正

--- Assets/Script/Editor/HighlightIfNullDrawer.cs
+++ Assets/Script/Editor/HighlightIfNullDrawer.cs
@@ -2,25 +2,21 @@ using UnityEditor;
 using UnityEngine;
 
 /// <summary>
-/// オブジェクトが未割当の場合、プロパティの背景色を変更する
+/// オブジェクトが未割当の場合プロパティの背景色を変更する
 /// </summary>
 [CustomPropertyDrawer(typeof(HighlightIfNullAttribute))]
 public class HighlightIfNullDrawer : PropertyDrawer
 {
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        Color defaultColor = GUI.backgroundColor; // デフォルトの背景色を保存
-        
-        bool isUnassigned = property.propertyType switch // 未割り当て（null）かどうかをチェック
-        {
-            SerializedPropertyType.ObjectReference => property.objectReferenceValue == null,
-            _ => false
-        };
+        // ObjectReference型で、値がnullの場合に背景色を変更
+        bool isNull = property.propertyType == SerializedPropertyType.ObjectReference && property.objectReferenceValue == null;
 
-        // 未割り当てなら背景色を変更
-        if (isUnassigned)
+        Color defaultColor = GUI.backgroundColor; // デフォルトの背景色を保存
+
+        if (isNull)
         {
-            GUI.backgroundColor = Color.red;
+            GUI.backgroundColor = Color.red; // 未割り当ての場合、背景色を赤に変更
         }
         
         EditorGUI.PropertyField(position, property, label); // プロパティを描画
