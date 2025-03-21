6ab50850f6a6fec2c1663b7aadfa71c3a2a16ac7
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-03-21 20:43:38 +0900

    Button属性をわかりやすいようにリファクタリング

--- Assets/Script/Attribute/MethodButtonInspectorAttribute.cs
+++ Assets/Script/Attribute/MethodButtonInspectorAttribute.cs
@@ -3,13 +3,14 @@ using System;
 
 /// <summary>
 /// インスペクターにボタンを表示し、メソッドを実行できるようにする属性
+/// （継承可能・同じメソッドに複数付けられない）
 /// </summary>
 [AttributeUsage(AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
-public class ButtonAttribute : PropertyAttribute
+public class MethodButtonInspectorAttribute : PropertyAttribute
 {
     public string Label { get; }
 
-    public ButtonAttribute(string label = null)
+    public MethodButtonInspectorAttribute(string label = null)
     {
         Label = label;
     }
--- Assets/Script/Editor/MethodButtonInspectorDrawer.cs
+++ Assets/Script/Editor/MethodButtonInspectorDrawer.cs
@@ -6,34 +6,33 @@ using System.Reflection;
 /// インスペクターにボタンを表示し、メソッドを実行できるようにします
 /// </summary>
 [CustomEditor(typeof(MonoBehaviour), true)]
-public class ButtonDrawer : Editor
+public class MethodButtonInspectorDrawer : Editor
 {
-    //動作を制御するフラグ
-    private static bool isEnabled = true;
-    
     public override void OnInspectorGUI()
     {
-        if (!isEnabled)
-        {
-            DrawDefaultInspector();
-            return;
-        }
-        
-        // デフォルトのプロパティを描画
-        DrawDefaultInspector();
+        DrawDefaultInspector(); // デフォルトのプロパティを描画
+        DrawButtonsForMethods(); // ボタンを描画
+    }
 
+    /// <summary>
+    /// メソッドを探してボタンを描画
+    /// </summary>
+    private void DrawButtonsForMethods()
+    {
         // 現在のターゲットオブジェクトを取得
         MonoBehaviour targetObject = (MonoBehaviour)target;
+        
+        // メソッドを取得
         MethodInfo[] methods = targetObject.GetType()
             .GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
 
         // [Button] 属性が付いたメソッドを探す
         foreach (MethodInfo method in methods)
         {
-            ButtonAttribute buttonAttribute = method.GetCustomAttribute<ButtonAttribute>();
-            if (buttonAttribute != null)
+            MethodButtonInspectorAttribute methodButtonInspectorAttribute = method.GetCustomAttribute<MethodButtonInspectorAttribute>();
+            if (methodButtonInspectorAttribute != null)
             {
-                string buttonText = string.IsNullOrEmpty(buttonAttribute.Label) ? method.Name : buttonAttribute.Label;
+                string buttonText = string.IsNullOrEmpty(methodButtonInspectorAttribute.Label) ? method.Name : methodButtonInspectorAttribute.Label;
 
                 // ボタンを描画
                 if (GUILayout.Button("MethodTest: " + buttonText))
