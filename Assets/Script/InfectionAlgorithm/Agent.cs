896c10bd97c25056637355f98771f8d6f3bd5847
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-03-24 10:01:15 +0900

    乱数生成方法を検討中

--- Assets/Script/InfectionAlgorithm/Agent.cs
+++ Assets/Script/InfectionAlgorithm/Agent.cs
@@ -13,10 +13,7 @@ public struct Agent
     public int Y { get; private set; }
     public AgentState State { get; private set; }
     public bool Skip{ get; set;}
-
-    // Randomは構造体に直接埋め込むのは難しいので、必要なときに生成する
-    private static Random _random = new Random();
-
+    
     public Agent(int id, AgentType type, int x, int y)
     {
         Id = id;
@@ -30,10 +27,10 @@ public struct Agent
     public void Move(Grid grid)
     {
         // 生活・逃走などのルールに応じた移動処理
-        int dx = _random.Next(-1, 2);
-        int dy = _random.Next(-1, 2);
-        X = Math.Max(0, Math.Min(grid.Areas.GetLength(0) - 1, X + dx));
-        Y = Math.Max(0, Math.Min(grid.Areas.GetLength(1) - 1, Y + dy));
+        // int dx = _random.Next(-1, 2);
+        // int dy = _random.Next(-1, 2);
+        // X = Math.Max(0, Math.Min(grid.Areas.GetLength(0) - 1, X + dx));
+        // Y = Math.Max(0, Math.Min(grid.Areas.GetLength(1) - 1, Y + dy));
     }
     
     /// <summary>
@@ -50,14 +47,5 @@ public struct Agent
     public void NearDeath()
     { 
         State = AgentState.NearDeath;
-    }
-
-    /// <summary>
-    /// 0-100の乱数を返す
-    /// </summary>
-    /// <returns></returns>
-    public int RandomNumber()
-    {
-        return _random.Next(0, 100);
     }
 }
--- Assets/Script/InfectionAlgorithm/MiniTest/MiniQuadtree.cs
+++ Assets/Script/InfectionAlgorithm/MiniTest/MiniQuadtree.cs
@@ -39,6 +39,7 @@ public class MiniQuadtree
     private float _difficultyMod; // 感染確率計算の難易度補正
     private int _infectionRange; // 感染範囲
     private HashSet<(int x, int y)> _coordsToMarkSkip = new HashSet<(int x, int y)>();
+    private System.Random _rand = new System.Random();
 
     public MiniQuadtree(Rect bounds, float regionMod, int depth = 0)
     {
@@ -486,6 +487,7 @@ public class MiniQuadtree
         {
             agents = _agentArray,
             infectionRate = infectionRate,
+            randomNumber = _rand.Next(0,100)
         };
         
         JobHandle jobHandle = job.Schedule(_agentArray.Length, 64);
@@ -507,6 +509,7 @@ public class MiniQuadtree
     {
         public NativeArray<Agent> agents;
         public float infectionRate;
+        public int randomNumber;
         
         public void Execute(int index)
         {
@@ -518,7 +521,7 @@ public class MiniQuadtree
             float infectionProbability = infectionRate * (1 - targetResistMod);
             
             // 感染判定。乱数が感染確率
-            if (infectionProbability > agent.RandomNumber())
+            if (infectionProbability > randomNumber)
             {
                 agent.Infect(); // 感染
             }
@@ -552,6 +555,7 @@ public class MiniQuadtree
         {
             agents = _nearDeathAgentsArray,
             lethalityRate = InfectionParameters.LethalityRate,
+            randomNumber = _rand.Next(0, 100)
         };
         
         JobHandle jobHandle = job.Schedule(_nearDeathAgentsArray.Length, 64);
@@ -579,12 +583,13 @@ public class MiniQuadtree
     {
         public NativeArray<Agent> agents;
         public float lethalityRate; // 致死率
+        public int randomNumber;
         
         public void Execute(int index)
         {
             Agent agent = agents[index];
 
-            if (lethalityRate > agent.RandomNumber())
+            if (lethalityRate > randomNumber)
             {
                 agent.NearDeath();
             }
