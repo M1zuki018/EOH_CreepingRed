f54a6f5632c1458154f39527100a1a9c053f7d21
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-04-03 15:24:21 +0900

    ランダムで感染を広げられるように変更

--- Assets/Script/Event/Infection_AcrossAreas.cs
+++ Assets/Script/Event/Infection_AcrossAreas.cs
@@ -1,25 +1,35 @@
-using Cysharp.Threading.Tasks;
+using Random = System.Random;
 
 /// <summary>
 /// エリアをまたぐ感染を管理するクラス
 /// </summary>
 public class Infection_AcrossAreas
 {
-    private Grid _grid; // Area情報を取得するためのGridクラスの参照
+    private readonly Grid _grid; // Area情報を取得するためのGridクラスの参照
+    private readonly Random _random;
+    private readonly int _rows; // Area二次元配列のヨコの長さ
+    private readonly int _cols; // Area二次元配列のタテの長さ
     
     public Infection_AcrossAreas(Grid grid)
     {
         _grid = grid;
-        SpreadEvent().Forget();
+        _random = new Random();
+        
+        // 二次元配列の長さを取得
+        _rows = _grid.Areas.GetLength(0);
+        _cols = _grid.Areas.GetLength(1);
+        
+        SpreadEvent();
     }
     
     /// <summary>
     /// 確率でエリアを跨いだ感染を発生させるクラス
     /// </summary>
-    private async UniTask SpreadEvent()
+    private void SpreadEvent()
     {
-        await UniTask.WaitForSeconds(3);
+        int x = _random.Next(_rows);
+        int y = _random.Next(_cols);
         
-        _grid.Areas[0,3].Spread();
+        _grid.Areas[x,y]?.Spread();
     }
 }
