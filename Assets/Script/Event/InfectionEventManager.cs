2a80d7ae5640b70a04b43213707cbcad8a148315
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-04-03 15:36:08 +0900

    一つのクラスに集約

--- Assets/Script/Event/InfectionEventManager.cs
+++ Assets/Script/Event/InfectionEventManager.cs
@@ -1,26 +1,12 @@
-using System;
-using R3;
-
 /// <summary>
 /// 感染イベント全体を管理するクラス
 /// </summary>
-public class InfectionEventManager : IDisposable
+public class InfectionEventManager
 {
     private Infection_AcrossAreas _acrossAreas; // エリアを跨ぐ感染を管理するクラス
-    private IDisposable _spreadEventObserver;
     
     public InfectionEventManager(Grid grid)
     {
         _acrossAreas = new Infection_AcrossAreas(grid);
-        
-        //TODO: 10秒に一度感染を広げるチェックを行う(この条件をあとで変更すること)
-        _spreadEventObserver = Observable
-            .Interval(TimeSpan.FromSeconds(10))
-            .Subscribe(_ => _acrossAreas.SpreadEvent());
-    }
-
-    public void Dispose()
-    {
-        _spreadEventObserver?.Dispose();
     }
 }
--- Assets/Script/Event/Infection_AcrossAreas.cs
+++ Assets/Script/Event/Infection_AcrossAreas.cs
@@ -1,14 +1,18 @@
+using System;
+using R3;
 using Random = System.Random;
 
 /// <summary>
 /// エリアをまたぐ感染を管理するクラス
 /// </summary>
-public class Infection_AcrossAreas
+public class Infection_AcrossAreas : IDisposable
 {
     private readonly Grid _grid; // Area情報を取得するためのGridクラスの参照
-    private readonly Random _random;
     private readonly int _rows; // Area二次元配列のヨコの長さ
     private readonly int _cols; // Area二次元配列のタテの長さ
+    
+    private readonly Random _random;
+    private readonly IDisposable _spreadEventObserver;
     
     public Infection_AcrossAreas(Grid grid)
     {
@@ -18,16 +22,26 @@ public class Infection_AcrossAreas
         // 二次元配列の長さを取得
         _rows = _grid.Areas.GetLength(0);
         _cols = _grid.Areas.GetLength(1);
+        
+        //TODO: 10秒に一度感染を広げるチェックを行う(この条件をあとで変更すること)
+        _spreadEventObserver = Observable
+            .Interval(TimeSpan.FromSeconds(10))
+            .Subscribe(_ => SpreadEvent());
     }
     
     /// <summary>
     /// 確率でエリアを跨いだ感染を発生させるクラス
     /// </summary>
-    public void SpreadEvent()
+    private void SpreadEvent()
     {
         int x = _random.Next(_rows);
         int y = _random.Next(_cols);
         
         _grid.Areas[x,y]?.Spread();
+    }
+
+    public void Dispose()
+    {
+        _spreadEventObserver?.Dispose();
     }
 }
